2d0d1c1a7a286b3d98369526dc5a56a6f9020792
venustuspl <venustus.pl@gmail.com>
2019-12-16 22:31:14 +0100

    0_1

---
+++ src/main/java/com/example/Quiz2020/Quiz2020Application.java
@@ -0,0 +1,13 @@
+package com.example.Quiz2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Quiz2020/Quiz2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quiz2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
