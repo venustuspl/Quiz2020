47e5f1682171ef8f4487a79d3af1978c4bfee319
venustuspl <venustus.pl@gmail.com>
2019-12-21 23:19:35 +0100

    0_6 Mappers

---
+++ src/main/java/com/example/Quiz2020/mapper/AnswerMapper.java
@@ -0,0 +1,39 @@
+package com.example.Quiz2020.mapper;
+
+import com.example.Quiz2020.domain.Answer;
+import com.example.Quiz2020.domain.AnswerDto;
+import com.example.Quiz2020.domain.Question;
+import com.example.Quiz2020.domain.QuestionDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AnswerMapper {
+
+        public Answer mapToAnswer(final AnswerDto answerDto){
+            return new Answer(
+                    answerDto.getAnswerId(),
+                    answerDto.getQuestionId(),
+                    answerDto.getAnswerInfo(),
+                    answerDto.getIsCorrect());
+        }
+
+
+        public AnswerDto mapToAnswerDto(final Answer answer){
+            return new AnswerDto(
+                    answer.getAnswerId(),
+                    answer.getQuestionId(),
+                    answer.getAnswerInfo(),
+                    answer.getCorrect()
+                    );
+        }
+
+        public List<AnswerDto> mapToAnswerDtoList(final List<Answer> answerList){
+            return answerList.stream()
+                    .map(q -> new AnswerDto(q.getAnswerId(),q.getQuestionId(),q.getAnswerInfo(),q.getCorrect()))
+                    .collect(Collectors.toList());
+        }
+    }
+
