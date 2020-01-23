acb12ed3b610af4e3863f7050e5029ebf0cbb76c
venustuspl <venustus.pl@gmail.com>
2020-01-23 21:02:44 +0100

    1_5 ModelMapperAdded

--- src/main/java/com/example/Quiz2020/controller/AnswerController.java
+++ src/main/java/com/example/Quiz2020/controller/AnswerController.java
@@ -1,13 +1,16 @@
 package com.example.Quiz2020.controller;
 
 
+import com.example.Quiz2020.domain.Answer;
 import com.example.Quiz2020.domain.AnswerDto;
 import com.example.Quiz2020.mapper.AnswerMapper;
 import com.example.Quiz2020.service.DbService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -15,41 +18,53 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/v1")
 public class AnswerController {
+
     @Autowired
     private AnswerMapper answerMapper;
 
     @Autowired
     private DbService service;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @RequestMapping(method = RequestMethod.GET, value = "/answers")
     public List<AnswerDto> getAnswers() {
-        return answerMapper.mapToAnswerDtoList(service.getAllAnswers());
+        List<AnswerDto> answers = service.getAllAnswers().stream()
+                .map(answer -> convertToAnswerDto(answer))
+                .collect(Collectors.toList());
+
+        return answers;
 
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/answer/{answerId}")
     public AnswerDto getAnswer(@PathVariable Long answerId) throws Exception {
 
-        return answerMapper.mapToAnswerDto(service.getAnswer(answerId).orElseThrow(Exception::new));
+        return convertToAnswerDto(service.getAnswer(answerId).orElseThrow(Exception::new));
 
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/allquestionanswers/{questionId}")
     public List<AnswerDto> getAllQuestionAnswers(@PathVariable int questionId) {
+        List<AnswerDto> answers = service.getAllAnswers().stream()
+                .map(answer -> convertToAnswerDto(answer))
+                .filter(answerDto -> answerDto.getQuestionId() == questionId)
+                .collect(Collectors.toList());
 
-        return answerMapper.mapToAnswerDtoList(service.getAllQuestionAnswers(questionId));
-
+        return answers;
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/answers/{answerId}")
     public void deleteAnswer(@PathVariable Long answerId) {
+
         service.deleteAnswer(answerId);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/answers")
     public AnswerDto updateAnswer(@RequestBody AnswerDto answerDto) {
 
-        return answerMapper.mapToAnswerDto(service.saveAnswer(answerMapper.mapToAnswer(answerDto)));
+        return convertToAnswerDto(service.saveAnswer(answerMapper.mapToAnswer(answerDto)));
     }
 
 
@@ -57,6 +72,16 @@ public class AnswerController {
     public void createAnswer(@RequestBody AnswerDto answerDto) {
 
         service.saveAnswer(answerMapper.mapToAnswer(answerDto));
+    }
+
+    private AnswerDto convertToAnswerDto(Answer answer) {
+        AnswerDto answerDto = modelMapper.map(answer, AnswerDto.class);
+        return answerDto;
+    }
+
+    private Answer convertToAnswerEntity(AnswerDto answerDto) {
+        Answer answer = modelMapper.map(answerDto, Answer.class);
+        return answer;
     }
 
 }
